691b5bf37650429fcb9338d2eed6eba1bf2695d7
Daniel Angell <me@danangell.com>
2023-09-03 16:07:49 -0700

    Add favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16" />
+        <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32" />
+      </Head>
       <body className="bg-gray-100">
         <Main />
         <NextScript />
